7441f97893f9ea015b12518052bee313da225a07
Thrive <guillaumemengelatte@hotmail.fr>
2023-03-01 10:07:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleSignup = async () => {
+    const randomEmail = Math.random().toString(36).substring(7) + '@user.dd';
+    console.log(randomEmail);
+
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "email": randomEmail,
+        "password": "passw",
+        "firstName": "John",
+        "lastName": "Doe"
+      })
+    });
+    const data = await response.json()
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={handleSignup}
+      >
+        Signup
+      </button>
     </div>
   );
 }
